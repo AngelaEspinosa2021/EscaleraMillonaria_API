550fbf99ee828b0648c2b73c5cbdc0c6ab07a839
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-12-21 09:58:26 -0500

    Creacion carpeta Data en el directorio raiz - creacion clase ApplicationDbContext.cs

---
+++ EscaleraMillonaria_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EscaleraMillonaria_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+    }
+}
