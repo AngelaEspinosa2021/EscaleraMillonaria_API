26bd8c772197b59c23f335b3b02c273878ffe724
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-12-21 10:04:39 -0500

    Creacion constructor dentro de la clase ApplicationDbContext - Llamado por medio de DbSet a nuestros modelos

--- EscaleraMillonaria_API/Data/ApplicationDbContext.cs
+++ EscaleraMillonaria_API/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿using EscaleraMillonaria_API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,5 +9,17 @@ namespace EscaleraMillonaria_API.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Question> Questions { get; set; }
+
+        public DbSet<Award> Awards { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Player> Players { get; set; }
     }
 }
