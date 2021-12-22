32ca0d6d902631a40afcc85727ceda43cadb86a1
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-12-22 12:20:48 -0500

    creacion carpeta Dto en el directorio raiz - creacion de la clase CategoryDto y ResponseDto

---
+++ EscaleraMillonaria_API/Models/Dto/CategoryDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EscaleraMillonaria_API.Models.Dto
+{
+    public class CategoryDto
+    {
+        public int IdCategory { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public List<Question> Questions { get; set; }
+
+        public List<Award> Awards { get; set; }
+
+    }
+}
