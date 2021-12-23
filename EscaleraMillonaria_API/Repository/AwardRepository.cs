4022ec8756f77811ab7353505282852b17f2f97e
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-12-23 09:12:02 -0500

    implementacion de las nuevas variables dentro del metodo cada metodo GetAwards() en AwardRepository - Creacion del metodo assignAward() en AwardRepository

--- EscaleraMillonaria_API/Repository/AwardRepository.cs
+++ EscaleraMillonaria_API/Repository/AwardRepository.cs
@@ -2,6 +2,7 @@
 using EscaleraMillonaria_API.Data;
 using EscaleraMillonaria_API.Models;
 using EscaleraMillonaria_API.Models.Dto;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,13 +21,33 @@ namespace EscaleraMillonaria_API.Repository
             _mapper = mapper;
         }
 
-        public Task<AwardDto> assignAward(int idCategory, int position)
+        public async Task<AwardDto> assignAward(int idCategory, int position)
         {
-            throw new NotImplementedException();
+            List<Award> allAwards = await _db.Awards.ToListAsync();
+            Award awardWon = new Award();
+
+            foreach (var award in allAwards)
+            {
+                int numPosition = Int32.Parse(award.QuestionPosition);
+                var temp = (from m in allAwards
+                            where m.IdCategory == idCategory && numPosition == position
+                            select m.AwardValue);
+
+                if(temp.Count() > 0)
+                {
+                    awardWon = (Award)temp;
+                }
+            }
+
+            return _mapper.Map<AwardDto>(awardWon);
+            
         }
 
-        //public async Task<List<AwardDto>> GetAwards()
-        //{
-        //    List<Award> list = await _db.Awards.
+        public async Task<List<AwardDto>> GetAwards()
+        {
+            List<Award> list = await _db.Awards.ToListAsync();
+
+            return _mapper.Map<List<AwardDto>>(list);
+        }
     }
 }
