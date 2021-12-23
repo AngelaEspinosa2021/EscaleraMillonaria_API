b46443e57dde0aa60de2f0f36dc4067e5ad8fea9
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-12-23 11:28:40 -0500

    creacion de la interfaz IPlayerRepository - creacion de PlayerDto dentro de MODELS

---
+++ EscaleraMillonaria_API/Models/Dto/PlayerDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EscaleraMillonaria_API.Models.Dto
+{
+    public class PlayerDto
+    {
+        public int IdPlayer { get; set; }
+
+        public string NamePlayer { get; set; }
+
+        public string PlayerAwards { get; set; }
+
+        public string PlayerCategory { get; set; }
+
+        public List<Question> QuestionsAnsweredByPlayer { get; set; }
+
+    }
+}
