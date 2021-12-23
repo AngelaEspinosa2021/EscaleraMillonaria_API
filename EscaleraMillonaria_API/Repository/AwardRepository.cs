c6bcb5814d9a7fe2172fd7ec7c422946cf31f729
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-12-23 08:42:34 -0500

    creacion e inicializacion de las variables para el ApplicationDbContext y IMapper dentro de AwardRepository

--- EscaleraMillonaria_API/Repository/AwardRepository.cs
+++ EscaleraMillonaria_API/Repository/AwardRepository.cs
@@ -1,4 +1,7 @@
-﻿using EscaleraMillonaria_API.Models.Dto;
+﻿using AutoMapper;
+using EscaleraMillonaria_API.Data;
+using EscaleraMillonaria_API.Models;
+using EscaleraMillonaria_API.Models.Dto;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +11,22 @@ namespace EscaleraMillonaria_API.Repository
 {
     public class AwardRepository : IAwardRepository
     {
+        private readonly ApplicationDbContext _db;
+        private IMapper _mapper;
+
+        public AwardRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
         public Task<AwardDto> assignAward(int idCategory, int position)
         {
             throw new NotImplementedException();
         }
 
-        public Task<List<AwardDto>> GetAwards()
-        {
-            throw new NotImplementedException();
-        }
+        //public async Task<List<AwardDto>> GetAwards()
+        //{
+        //    List<Award> list = await _db.Awards.
     }
 }
