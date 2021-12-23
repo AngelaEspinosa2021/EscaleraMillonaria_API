aa6f96cc92ff589868720b08654f4dfef6cfaaa6
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-12-23 09:16:19 -0500

    creacion de AwardController y eliminacion de metodos innecesarios

---
+++ EscaleraMillonaria_API/Controllers/AwardsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EscaleraMillonaria_API.Data;
+using EscaleraMillonaria_API.Models;
+
+namespace EscaleraMillonaria_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AwardsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AwardsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Awards
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Award>>> GetAwards()
+        {
+            return await _context.Awards.ToListAsync();
+        }
+
+        private bool AwardExists(int id)
+        {
+            return _context.Awards.Any(e => e.IdAward == id);
+        }
+    }
+}
