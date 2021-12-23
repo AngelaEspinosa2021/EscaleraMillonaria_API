19ed0d5cda932c37634f8932ab8c2a48c441d8fe
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-12-22 23:34:47 -0500

    Creacion de QuestionsController y eliminacion de metodos innecesarios

---
+++ EscaleraMillonaria_API/Controllers/QuestionsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EscaleraMillonaria_API.Data;
+using EscaleraMillonaria_API.Models;
+
+namespace EscaleraMillonaria_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public QuestionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Questions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Question>>> GetQuestions()
+        {
+            return await _context.Questions.ToListAsync();
+        }
+
+        // GET: api/Questions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Question>> GetQuestion(int id)
+        {
+            var question = await _context.Questions.FindAsync(id);
+
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            return question;
+        }       
+
+        private bool QuestionExists(int id)
+        {
+            return _context.Questions.Any(e => e.IdQuestion == id);
+        }
+    }
+}
